844ea8b29166a356041ba6a4880e7ee611910f70
SShak <shawnshaknitz@gmail.com>
2022-01-23 11:23:35 -0500

    push it real good

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -5,34 +5,37 @@ export default function FeaturedInfo() {
     return (
         <div className="featured">
             <div className="featuredItem">
-                <span className="featuredTitle">Revanue</span>
-                <div className="featuredMoneyContainer"></div>
+                <span className="featuredTitle">Revenue</span>
+                <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$1,000</span>
                     <span className="featuredMoneyRate">
-                        -10.5 <ArrowDownward />
+                        -10.5 <ArrowDownward className="featuredIcon negative"/>
                     </span>
+                    </div>    
                 <div>
             </div>        
                     <span className="featuredSub">Compared to last month</span>
                 </div>
             <div className="featuredItem">
                 <span className="featuredTitle">Sales</span>
-                <div className="featuredMoneyContainer"></div>
+                <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$5,000</span>
                     <span className="featuredMoneyRate">
-                        -1.5 <ArrowDownward />
+                        -1.5 <ArrowDownward className="featuredIcon negative"/>
                     </span>
+                </div>    
                 <div>
             </div>        
                     <span className="featuredSub">Compared to last month</span>
                 </div> 
             <div className="featuredItem">
                 <span className="featuredTitle">Cost</span>
-                <div className="featuredMoneyContainer"></div>
+                <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$750</span>
                     <span className="featuredMoneyRate">
-                        +2.5 <ArrowUpward />
+                        +2.5 <ArrowUpward className="featuredIcon"/>
                     </span>
+                </div>    
                 <div>
                     <span className="featuredSub">Compared to last month</span>
                 </div> 
